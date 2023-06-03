d3b417777736d69f217d689b42170889c6eb5b8c
jonmsawyer <jonmsawyer@gmail.com>
2023-06-03 09:45:32 -0600

    Update unit test.

--- lib/chui_core/tests/init_engine.rs
+++ lib/chui_core/tests/init_engine.rs
@@ -17,16 +17,20 @@ fn init_engine() {
             "White: Nathan Vander Martin (Age 36) (no Elo rating)
 Black: Shop Guy. Bob Vila III (Age 57) (987 Elo)
 Position:
-8 | r  n  b  q  k  b  n  r
-7 | p  p  p  p  p  p  p  p
-6 | ·  ·  ·  ·  ·  ·  ·  ·
-5 | ·  ·  ·  ·  ·  ·  ·  ·
-4 | ·  ·  ·  ·  ·  ·  ·  ·
-3 | ·  ·  ·  ·  ·  ·  ·  ·
-2 | P  P  P  P  P  P  P  P
-1 | R  N  B  Q  K  B  N  R
-  +-----------------------
-    a  b  c  d  e  f  g  h
+╔═════════════════════════╗
+║     a b c d e f g h     ║
+║   ┌─────────────────┐   ║
+║ 8 │ r n b q k b n r │ 8 ║
+║ 7 │ p p p p p p p p │ 7 ║
+║ 6 │ · · · · · · · · │ 6 ║
+║ 5 │ · · · · · · · · │ 5 ║
+║ 4 │ · · · · · · · · │ 4 ║
+║ 3 │ · · · · · · · · │ 3 ║
+║ 2 │ P P P P P P P P │ 2 ║
+║ 1 │ R N B Q K B N R │ 1 ║
+║   └─────────────────┘   ║
+║     a b c d e f g h   \u{1b}[1;33m♔\u{1b}[0m ║
+╚═════════════════════════╝
 White to move."
         );
     }
